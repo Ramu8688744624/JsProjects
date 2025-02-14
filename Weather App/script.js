9763b6dbdee2e425a1d6ddb96db5dcb846a3e403
Ramu A <ramua@VIVID.AD>
2025-02-14 16:37:19 +0530

    Updated weather app

---
+++ Weather App/script.js
@@ -0,0 +1,35 @@
+const searchBtn=document.getElementById("searchBtn");
+const inputText=document.getElementById("cityInput");
+const temperature=document.getElementById("temperature");
+const cityName=document.getElementById("cityName");
+const humidityValue=document.getElementById("humidity");
+const windSpeedValue=document.getElementById("windSpeed");
+
+
+searchBtn.addEventListener('click',async () => {
+const city=inputText.value.trim();
+if(city){
+    const resonse=await getData(city);
+    updateWeatherUI(resonse);
+}
+
+});
+
+async function getData(city) {
+    const apiKey="dc6f259fc990d1e2485706724ebc9b13";
+    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+    const data=await response.json(); 
+    return data;
+}
+
+function updateWeatherUI(data) {
+    const {main,wind,name}=data;
+    const tempratureValue=main.temp;
+    const windSpeed=wind.speed;
+    const humidity=main.humidity;
+
+    temperature.textContent=`${Math.round(tempratureValue)}°C`
+    cityName.textContent=name;
+    humidityValue.textContent=`${humidity}%`;
+    windSpeedValue.textContent=`${windSpeed}km/h`;
+}
